ca3eb1f173ac9d6a80eff79a3c900571867392e7
jgkin <himanthagamachchige@gmail.com>
2025-02-11 08:28:47 +0530

    error fixed

--- _11ApiBackend/src/main/java/lk/ijse/controller/CustomerController.java
+++ _11ApiBackend/src/main/java/lk/ijse/controller/CustomerController.java
@@ -5,7 +5,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
-@CrossOrigin("*")
+@CrossOrigin(origins = "http://localhost:63342")
+
 @RestController
 @RequestMapping("api/v1/customer")
 public class CustomerController {
